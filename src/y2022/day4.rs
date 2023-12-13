9784a72242e3f1e5537756bdd10c54186c224cdd
muksterz <67229252+muksterz@users.noreply.github.com>
2023-12-13 13:46:57 -0500

    2022 day4

---
+++ src/y2022/day4.rs
@@ -0,0 +1,85 @@
+
+
+use runner::aoc;
+
+type Range = std::ops::RangeInclusive<u64>;
+
+
+fn parse_range(input: &str) -> (Range, Range) {
+    fn parse(input: &str) -> Range {
+        let (l, r) = input.trim().split_once('-').unwrap();
+
+        let l = u64::from_str_radix(l, 10).unwrap();
+        let r = u64::from_str_radix(r, 10).unwrap();
+        l..=r
+    }
+
+    let (l, r) = input.trim().split_once(',').unwrap();
+
+    (parse(l), parse(r))
+}
+
+#[aoc(day4, part1)]
+fn part1(input: &str) -> u64 {
+
+    let mut total = 0;
+
+    for l in input.trim().lines().map(str::trim) {
+        let (l, r) = parse_range(l);
+        if (l.contains(r.start()) && l.contains(r.end())) || (r.contains(l.start()) && r.contains(l.end())) {
+            total += 1;
+        }
+    }
+
+    total
+
+}
+
+
+#[aoc(day4, part2)]
+fn part2(input: &str) -> u64 {
+
+    let mut total = 0;
+
+    for l in input.trim().lines().map(str::trim) {
+        let (l, r) = parse_range(l);
+        if l.contains(r.start()) || l.contains(r.end()) || r.contains(l.start()) || r.contains(l.end()) {
+            total += 1;
+        }
+    }
+
+    total
+
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn part1() {
+        let input = "
+            2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8
+        ".trim();
+
+        assert_eq!(super::part1(input), 2);
+    }
+
+    #[test]
+    fn part2() {
+        let input = "
+            2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8
+        ".trim();
+
+        assert_eq!(super::part2(input), 4);
+    }
+}
--- src/y2022/mod.rs
+++ src/y2022/mod.rs
@@ -3,3 +3,4 @@ runner::aoc_year!(2022);
 mod day1;
 mod day2;
 mod day3;
+mod day4;
